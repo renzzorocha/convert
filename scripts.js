acf071fe66ef80f61783afdb29244fa97046bd7c
Renzzo Rocha <renzzorocha17@gmail.com>
2025-11-22 17:53:32 -0300

    finish.

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol) {
+    try {
+        description.textContent = `${symbol} 1  = ${formatCurrency(price)}`
+        let total = amount * price
+        total = formatCurrency(total).replace("R$", "")
+        result.textContent = `${total} Reais`
+        footer.classList.add("show-result")
+    } catch (error) {
+        footer.classList.remove("show-result")
+        console.log(error)
+        alert("Nao foi possivel realizar a conversao.")
+    }
+}
+
+function formatCurrency(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+}
+
+
